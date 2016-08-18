293eb189c06d37818dbdfcbde3beec33cef9148e
George Thomas <georgetamaothomas@gmail.com>
2016-08-18 22:43:12 +0100

    Add fetchCommutes

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
+const pubStravaToken = "57a2e0bbc8a2789e2a56cb2f911d76d6ce48b5e5";
+
 let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 51.508235, lng: -0.324592},
-        zoom: 8,
+        zoom: 12,
     });
 
     fetch('679046175.json')
@@ -22,4 +24,21 @@ function makeGeoJson(stream) {
             coordinates: stream.map(s => [s.point.lng, s.point.lat]),
         }
     };
+}
+
+function fetchCommutes(direction) {
+    headers = new Headers();
+    headers.set("Authorization", "Bearer " + pubStravaToken);
+
+    const p = fetch("https://www.strava.com/api/v3/athlete/activities", headers)
+        .then(x => x.json())
+        
+    if (direction === "to_work") {
+        // get the morning commutes
+        return p.then(acts => acts.filter(a => new Date(a.start_date).getHours() < 12))
+    } else if (direction === "from_work") {
+        // get the evening commutes
+        return p.then(acts => acts.filter(a => new Date(a.start_date).getHours() >= 12))
+    }
+    return p
 }
